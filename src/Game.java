5b36e174b7630e2d084aa585ed167b1cf8f0b4a8
Emilien Guenichon <e.guenichon@gmail.com>
2018-08-13 23:20:41 +0200

    Improve logger, actions and start battle engine.

--- src/Game.java
+++ src/Game.java
@@ -2,44 +2,51 @@ import java.util.*;
 
 class Player {
 
-    private static final Logger LOG = Logger.getInstance();
+    private static final Channel LOG = Channel.getInstance();
 
     private static int roundNumber = 1;
 
     public static void main(String args[]) {
 
+        Channel.DEBUG_ENABLED = false;
+
         Scanner in = new Scanner(System.in);
 
         LOG.debug("Game start ...");
+
+        Engine engine = new EfficiencyDraftEngine();
 
         while (true) {
 
             Round round = RoundParser.parseRound(in);
             LOG.info("Round status : %s", round);
 
-            if (roundNumber <= 30) {
-                DraftEngine.processRound(round);
-            } else {
-                GameEngine.processRound(round);
+            engine.processRound(round);
+
+            if (roundNumber == 30) {
+                LOG.info("Round 30 switching to game engine");
+                engine = new GameEngine(round.cardList);
             }
 
             LOG.debug("End of turn " + roundNumber);
+
             roundNumber++;
         }
     }
 }
 
-class DraftEngine {
+class EfficiencyDraftEngine implements Engine {
 
-    private static final Logger LOG = Logger.getInstance();
+    private static final Channel CHANNEL = Channel.getInstance();
 
-    public static void processRound(Round round) {
+    @Override
+    public void processRound(Round round) {
         Card selectedCard = null;
         float maxEfficiency = Float.MAX_VALUE;
         int selectedIdx = -1;
         int idx = 0;
         for (Card card : round.cardList) {
-            float costEfficiency = card.costEfficiency();
+            float costEfficiency = costEfficiency(card);
             if (costEfficiency < maxEfficiency) {
                 selectedCard = card;
                 maxEfficiency = costEfficiency;
@@ -48,12 +55,16 @@ class DraftEngine {
             idx++;
         }
 
-        LOG.info("Picking card %s at idx %d", selectedCard.toString(), selectedIdx);
+        CHANNEL.info("Picking card %s at idx %d", selectedCard.toString(), selectedIdx);
         pickCard(selectedIdx);
     }
 
-    private static void pass() {
-        System.out.println("PASS");
+    public float costEfficiency(Card card) {
+        if (!card.hasHabilities()) {
+            return (card.attack + card.defense) / card.cost;
+        } else {
+            return 1;
+        }
     }
 
     private static void pickCard(int number) {
@@ -61,10 +72,77 @@ class DraftEngine {
     }
 }
 
-class GameEngine {
-    public static void processRound(Round round) {
+class GameEngine implements Engine {
 
+    private static final Channel CHANNEL = Channel.getInstance();
+
+    private List<Card> deck;
+
+    private List<Card> hand;
+
+    private List<Card> board = new ArrayList<>();
+    private List<Card> opponentBoard = new ArrayList<>();
+
+    public GameEngine(List<Card> deck) {
+        this.deck = deck;
     }
+
+    @Override
+    public void processRound(Round round) {
+        this.hand = round.cardList;
+        this.hand.removeAll(board);
+
+        CHANNEL.info("Hand : %s", hand.toString());
+        CHANNEL.info("Board : %s", board.toString());
+
+        List<Action> actionList = new ArrayList<>();
+
+        // Invoke
+        actionList.addAll(bestSummonAction(round));
+
+        // Attack
+        actionList.addAll(bestAttackAction(round));
+
+        play(actionList);
+    }
+
+    private void play(List<Action> actionList) {
+        if (actionList.isEmpty()) {
+            actionList.add(new Pass());
+        }
+
+        CHANNEL.play(actionList);
+    }
+
+    private List<Action> bestSummonAction(Round round) {
+        List<Action> actionList = new ArrayList<>();
+        int spentMana = 0;
+
+        // Remove already played cards
+        round.cardList.removeAll(board);
+        // Sort decreasing cost order
+        round.cardList.sort(Comparator.comparingInt((Card o) -> o.cost).reversed());
+
+        for (Card card : round.cardList) {
+            if (card.cost < round.current.playerMana - spentMana) {
+                spentMana += card.cost;
+                board.add(card);
+                actionList.add(new Summon(card.instanceId));
+            }
+        }
+        return actionList;
+    }
+
+    private List<Action> bestAttackAction(Round round) {
+        List<Action> actionList = new ArrayList<>();
+
+        return actionList;
+    }
+
+}
+
+interface Engine {
+    void processRound(Round round);
 }
 
 class Round {
@@ -152,14 +230,6 @@ class Card {
         this.cardDraw = in.nextInt();
     }
 
-    public float costEfficiency() {
-        if (!hasHabilities()) {
-            return (attack + defense) / cost;
-        } else {
-            return 1;
-        }
-    }
-
     @Override
     public String toString() {
         String result = simpleString();
@@ -175,7 +245,7 @@ class Card {
     }
 
     public String simpleString() {
-        return String.format("[M%d,A%d,D%d]", cost, attack, defense);
+        return String.format("[id%d; M%d, A%d, D%d]", instanceId, cost, attack, defense);
     }
 
     public boolean hasHabilities() {
@@ -185,26 +255,20 @@ class Card {
 
 class RoundParser {
 
-    private static final Logger LOG = Logger.getInstance();
+    private static final Channel LOG = Channel.getInstance();
 
     public static Round parseRound(Scanner in) {
 
         PlayerStatus current = new PlayerStatus(in);
         PlayerStatus opponent = new PlayerStatus(in);
 
-//        LOG.debug("Current player : " + current);
-//        LOG.debug("Opponent player : " + current);
-
         int opponentHand = in.nextInt();
         int cardCount = in.nextInt();
-
-//        LOG.debug("Opponent hand " + opponentHand);
-//        LOG.debug("Card count " + cardCount);
 
         List<Card> cardList = new ArrayList<>();
         for (int i = 0; i < cardCount; i++) {
             Card card = new Card(in);
-            LOG.info("Card %s", card.toString());
+            LOG.debug("Card %s", card.toString());
             cardList.add(card);
         }
 
@@ -212,22 +276,94 @@ class RoundParser {
     }
 }
 
-class Logger {
+class Channel {
 
-    private static Logger INSTANCE = new Logger();
-    private static boolean debugEnabled = true;
+    private static Channel INSTANCE = new Channel();
+    public static boolean DEBUG_ENABLED = true;
 
-    public static Logger getInstance() {
+    public static Channel getInstance() {
         return INSTANCE;
     }
 
     public void info(String message, Object... args) {
-        System.err.println(String.format(message, args));
+        System.err.println("Info: " + String.format(message, args));
+    }
+
+    public void info(String message, List<Object> args) {
+        System.err.println("Info: " + String.format(message, args.toString()));
     }
 
     public void debug(String message, Object... args) {
-        if (debugEnabled) {
-            System.err.println(String.format(message, args));
+        if (DEBUG_ENABLED) {
+            System.err.println("Debug: " + String.format(message, args));
         }
+    }
+
+    public void debug(String message, List<Object> args) {
+        if (DEBUG_ENABLED) {
+            System.err.println("Debug: " + String.format(message, args));
+        }
+    }
+
+    public void play(Action... actions) {
+        info("Play actions : " + join(actions));
+        System.out.println(join(actions));
+    }
+
+    public void play(List<Action> actions) {
+        this.play(actions.toArray(new Action[actions.size()]));
+    }
+
+    private String join(Action... actions) {
+        if (actions.length == 1) {
+            return actions[0].toString();
+        } else {
+            StringBuilder builder = new StringBuilder();
+            for (Action action : actions) {
+                builder.append(action.toString()).append(";");
+            }
+            return builder.toString();
+        }
+    }
+}
+
+abstract class Action {
+    private String action;
+
+    public Action(String action) {
+        this.action = action;
+    }
+
+    @Override
+    public String toString() {
+        return action;
+    }
+}
+
+class Pass extends Action {
+    public Pass() {
+        super("PASS");
+    }
+}
+
+class Pick extends Action {
+    public Pick(int cardNumber) {
+        super("PICK " + cardNumber);
+    }
+}
+
+class Summon extends Action {
+    public Summon(int id) {
+        super("SUMMON " + id);
+    }
+}
+
+class Attack extends Action {
+    public Attack(int id) {
+        super("ATTACK " + id);
+    }
+
+    public Attack(int id, int targetId) {
+        super("ATTACK " + id + " " + targetId);
     }
 }
